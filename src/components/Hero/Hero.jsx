0170f6712fd85cd2f414f2303da09afeb7fa1d55
Volodymyr Fedorenko <vovafedorenko1@gmail.com>
2024-12-22 12:34:29 +0200

    today

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+
+const Hero = () => {
+    return (
+        <section className="hero-wrapper">
+            <div className="paddings innerWidth flexCenter hero-container">
+
+            </div>
+        </section>
+    )
+}
+
+
+export default Hero;
