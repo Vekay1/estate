f106569c38affcb24a5ffc6c9f57b61dbd537746
Volodymyr Fedorenko <vovafedorenko1@gmail.com>
2024-12-22 12:53:18 +0200

    pishov kushac

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,14 @@ const Hero = () => {
     return (
         <section className="hero-wrapper">
             <div className="paddings innerWidth flexCenter hero-container">
-
+                <div className="hero-left">
+                    Left section
+                </div>
+                <div className="flexCenter hero-right">
+                    <div className="image-conatiner">
+                        <img src="/hero-image.png" alt="hero"/>
+                    </div>
+                </div>
             </div>
         </section>
     )
