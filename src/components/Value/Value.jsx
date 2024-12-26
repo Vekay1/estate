1e36d5f77b386fd92e4d3ecbb89c4dd6b5c7d65e
Volodymyr Fedorenko <vovafedorenko1@gmail.com>
2024-12-26 16:26:20 +0200

    Update Value.jsx

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -36,7 +36,7 @@ const Value = () => {
                     >
                         {data.map((item, i) => {
                             return (
-                                <AccordionItem className="accordionItem" key={i} uuid=>
+                                <AccordionItem className="accordionItem" key={i} uuid=  gh>
                                     <AccordionItemHeading>
                                         <AccordionItemButton>
                                             <div className="flexCenter icon">{item.icon}</div>
