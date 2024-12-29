68eabceaf57a661692a1ead1a980a94b1c535e14
Volodymyr Fedorenko <vovafedorenko1@gmail.com>
2024-12-29 10:35:05 +0200

    пам

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
         <section className="c-wrapper">
             <div className="paddings innerWidth flexCenter c-container">
                 {/* left side */}
-                <div className="c-left">
+                <div className="flexColStart c-left">
                     <span className="orangeText">Our Contacts</span>
                     <span className="primaryText">Easy to Contact us</span>
                     <span className="secondaryText">
@@ -13,13 +13,28 @@ const Contact = () => {
                         the best services.
                         We believe a good place to live can make
                         your life better.
+
+                        <div className="flexColStart contactModes">
+                            {/* first row */}
+                            <div className="flexColStart row">
+                            <div className="flexColCenter mode">
+                            <div className="flexStart">
+                                <div className="flexCenter icon">
+
+                                </div>
+                             </div>
+                          </div>
+                         </div>
+                        </div>
+
+
                     </span>
                 </div>
 
                 {/* right side */}
                 <div className="c-right">
                     <div className="image-container">
-                        <img src="./contact.jpg" alt="" />
+                        <img src="./contact.jpg" alt=""/>
                     </div>
                 </div>
 
