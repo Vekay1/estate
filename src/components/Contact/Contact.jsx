6f6b86f66a7835af042914871d98fa77408e7936
Volodymyr Fedorenko <vovafedorenko1@gmail.com>
2025-01-03 20:41:27 +0200

    ен

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,8 @@
 import './Contact.css';
+import { MdCall } from 'react-icons/md';
+import { BsFillChatDotsFill } from 'react-icons/bs';
+import { HiChatBubbleBottomCenter } from 'react-icons/hi2';
+
 
 const Contact = () => {
     return (
@@ -16,31 +20,90 @@ const Contact = () => {
 
                         <div className="flexColStart contactModes">
                             {/* first row */}
-                            <div className="flexColStart row">
+                            <div className="flexStart row">
+                               <div className="flexColCenter mode">
+                                 <div className="flexStart">
+                                   <div className="flexCenter icon">
+                                      <MdCall size={25}/>
+                                  </div>
+                                     <div className="flexColStart detail">
+                                    <span className="primaryText">Call</span>
+                                    <span className="secondaryText">021 123 145 12</span>
+                                     </div>
+                             </div>
+                                <div className="flexCenter button">
+                                    Call Now
+                                </div>
+                          </div>
+                         </div>
+                        <div className="flexStart row">
                             <div className="flexColCenter mode">
                             <div className="flexStart">
                                 <div className="flexCenter icon">
-
+                                    <BsFillChatDotsFill size={25}/>
+                                </div>
+                                <div className="flexColStart detail">
+                                    <span className="primaryText">Chat</span>
+                                    <span className="secondaryText">021 123 145 12</span>
                                 </div>
                              </div>
+                                <div className="flexCenter button">
+                                    Call Now
+                                </div>
                           </div>
                          </div>
+                         </div>
+                        <div className="flexStart row">
+                        <div className="flexColCenter mode">
+                            <div className="flexStart">
+                                <div className="flexCenter icon">
+                                    <MdCall size={25}/>
+                                </div>
+                                <div className="flexColStart detail">
+                                    <span className="primaryText">Video Call </span>
+                                    <span className="secondaryText">021 123 145 12</span>
+                                </div>
+                             </div>
+                                <div className="flexCenter button">
+                                    Video Call Now
+                                </div>
+                          </div>
+                          </div>
+
+                <div className="flexStart row">
+                <div className="flexColCenter mode">
+                    <div className="flexStart">
+                        <div className="flexCenter icon">
+                            <HiChatBubbleBottomCenter size={25}/>
                         </div>
-
-
-                    </span>
-                </div>
-
-                {/* right side */}
-                <div className="c-right">
-                    <div className="image-container">
-                        <img src="./contact.jpg" alt=""/>
+                        <div className="flexColStart detail">
+                            <span className="primaryText">Message</span>
+                            <span className="secondaryText">021 123 145 12</span>
+                        </div>
+                    </div>
+                    <div className="flexCenter button">
+                        Message Now
                     </div>
                 </div>
+                </div>
 
-            </div>
-        </section>
-    );
+
+
+        </span>
+                </div>
+
+                {/* right side */
+                }
+    <div className="c-right">
+        <div className="image-container">
+            <img src="./contact.jpg" alt=""/>
+        </div>
+    </div>
+
+</div>
+</section>
+)
+    ;
 }
 
 export default Contact;
